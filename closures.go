aea476d1cf4f7ba53282000732ad25486083502e
bovem <av_nish@outlook.com>
2023-06-11 11:14:37 +0530

    Completed methods

---
+++ closures.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func intSequence() func() int{
+	i:=0
+	return func() int{
+		i++
+		return i
+	}
+}
+
+func main() {
+	nextInt := intSequence()
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+
+	nextInts := intSequence()
+	fmt.Println(nextInts())
+}
